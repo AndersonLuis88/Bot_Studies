4aa2155b780e088be9b1f4c460f8bec83a8b6c18
Anderson <andersonluis1486@gmail.com>
2019-06-06 19:09:50 -0300

    Criado classe Instagram, para captura de dados da página do usuário!

--- Bot_Instagram/Bot_Instagram/Instagram.cs
+++ Bot_Instagram/Bot_Instagram/Instagram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace Bot_Instagram
@@ -10,6 +11,48 @@ namespace Bot_Instagram
         {
             var profile = new Profile(username);
             string url = @"https://www.instagram.com/"+username"/";
+            string code;
+            using (WebClient a = new WebClient())
+            {
+                code = a.DownloadString(url);
+            }
+            HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+            html.LoadHtml(code);
+
+            var list = html.DocumentNode.SelectNodes("//meta");
+
+            foreach (var node in list)
+            {
+                string property = node.GetAttributeValue("property", "");
+
+                if(property == "al:ios:app_name")
+                {
+                    profile.IosAppName = node.GetAttributeValue("content", "");
+                }
+
+                if (property == "al:ios:app_store_id")
+                {
+                    profile.IosAppId = node.GetAttributeValue("content", "");
+                }
+
+                if (property == "al:ios:url")
+                {
+                    profile.IosUrl = node.GetAttributeValue("content", "");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
