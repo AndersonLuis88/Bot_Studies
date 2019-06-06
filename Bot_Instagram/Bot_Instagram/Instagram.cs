b03ed135e47f7d08e68e49b580426be9be1f7b00
Anderson <andersonluis1486@gmail.com>
2019-06-06 19:07:36 -0300

    Inicio do projeto, Web scrapping instagram

---
+++ Bot_Instagram/Bot_Instagram/Instagram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot_Instagram
+{
+    public static class Instagram
+    {
+        public static Profile GetProfileByUser(string username)
+        {
+            var profile = new Profile(username);
+            string url = @"https://www.instagram.com/"+username"/";
+        }
+    }
+}
---
+++ Bot_Instagram/Bot_Instagram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bot_Instagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
