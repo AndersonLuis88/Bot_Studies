243a03ba505b03aa847ff065b4f34fb7d08df5e8
Anderson <andersonluis1486@gmail.com>
2019-06-11 15:21:18 -0300

    Add projeto, bot para views no youtube

---
+++ Bot_Instagram/Bot_Youtube_View_Selenium/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using prmToolkit.Configuration;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+using System;
+using System.Threading;
+
+namespace Bot_Youtube_View_Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = WebDriverFactory.CreateWebDriver(Browser.Chrome, @"E:\00.ESTUDOS\00.GITHUB");
+
+            try
+            {
+                //Link do video!
+                webDriver.LoadPage(TimeSpan.FromSeconds(20), "http://www.youtube.com/video");
+
+                IWebElement btnPularProp = null;
+                try
+                {
+                    //Pegar a tag do botão de fechar anúncio
+                    btnPularProp = webDriver.FindElement(By.ClassName("videoAdUiFixedPaddingSkipButtonText"));                                        
+                    Console.WriteLine("Fechando Anuncio...");
+                    btnPularProp.Click();                                     
+
+                    Thread.Sleep(TimeSpan.FromSeconds(20));
+                }
+                catch (NoSuchElementException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                webDriver.Close();
+                webDriver.Dispose();
+            }
+
+
+                Console.ReadKey();
+        }
+    }
+}
